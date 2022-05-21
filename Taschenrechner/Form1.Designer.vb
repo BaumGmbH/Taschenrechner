c4c0cc2aa0f3296b840c947407a82547316e9727
psyGamer <psygamer21@gmail.com>
2022-05-21 19:49:47 +0200

    Initial commit

---
+++ Taschenrechner/Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tb = New System.Windows.Forms.TextBox()
+        Me.b7 = New System.Windows.Forms.Button()
+        Me.b8 = New System.Windows.Forms.Button()
+        Me.b9 = New System.Windows.Forms.Button()
+        Me.b6 = New System.Windows.Forms.Button()
+        Me.b5 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b0 = New System.Windows.Forms.Button()
+        Me.bDot = New System.Windows.Forms.Button()
+        Me.bDel = New System.Windows.Forms.Button()
+        Me.bAC = New System.Windows.Forms.Button()
+        Me.bDiv = New System.Windows.Forms.Button()
+        Me.bMulti = New System.Windows.Forms.Button()
+        Me.bMinus = New System.Windows.Forms.Button()
+        Me.bPlus = New System.Windows.Forms.Button()
+        Me.bEnter = New System.Windows.Forms.Button()
+        Me.bRound = New System.Windows.Forms.Button()
+        Me.b1x = New System.Windows.Forms.Button()
+        Me.bPI = New System.Windows.Forms.Button()
+        Me.bE = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'tb
+        '
+        Me.tb.Location = New System.Drawing.Point(24, 26)
+        Me.tb.Name = "tb"
+        Me.tb.Size = New System.Drawing.Size(245, 20)
+        Me.tb.TabIndex = 0
+        '
+        'b7
+        '
+        Me.b7.Location = New System.Drawing.Point(24, 69)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(40, 40)
+        Me.b7.TabIndex = 1
+        Me.b7.Text = "7"
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Location = New System.Drawing.Point(70, 69)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(40, 40)
+        Me.b8.TabIndex = 2
+        Me.b8.Text = "8"
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Location = New System.Drawing.Point(116, 69)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(40, 40)
+        Me.b9.TabIndex = 3
+        Me.b9.Text = "9"
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Location = New System.Drawing.Point(116, 115)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(40, 40)
+        Me.b6.TabIndex = 6
+        Me.b6.Text = "6"
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Location = New System.Drawing.Point(70, 115)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(40, 40)
+        Me.b5.TabIndex = 5
+        Me.b5.Text = "5"
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(24, 115)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(40, 40)
+        Me.b4.TabIndex = 4
+        Me.b4.Text = "4"
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(116, 161)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(40, 40)
+        Me.b3.TabIndex = 9
+        Me.b3.Text = "3"
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(70, 161)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(40, 40)
+        Me.b2.TabIndex = 8
+        Me.b2.Text = "2"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(24, 161)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(40, 40)
+        Me.b1.TabIndex = 7
+        Me.b1.Text = "1"
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'b0
+        '
+        Me.b0.Location = New System.Drawing.Point(70, 207)
+        Me.b0.Name = "b0"
+        Me.b0.Size = New System.Drawing.Size(40, 40)
+        Me.b0.TabIndex = 11
+        Me.b0.Text = "0"
+        Me.b0.UseVisualStyleBackColor = True
+        '
+        'bDot
+        '
+        Me.bDot.Location = New System.Drawing.Point(24, 207)
+        Me.bDot.Name = "bDot"
+        Me.bDot.Size = New System.Drawing.Size(40, 40)
+        Me.bDot.TabIndex = 10
+        Me.bDot.Text = ","
+        Me.bDot.UseVisualStyleBackColor = True
+        '
+        'bDel
+        '
+        Me.bDel.Location = New System.Drawing.Point(183, 69)
+        Me.bDel.Name = "bDel"
+        Me.bDel.Size = New System.Drawing.Size(40, 40)
+        Me.bDel.TabIndex = 12
+        Me.bDel.Text = "Del"
+        Me.bDel.UseVisualStyleBackColor = True
+        '
+        'bAC
+        '
+        Me.bAC.Location = New System.Drawing.Point(229, 69)
+        Me.bAC.Name = "bAC"
+        Me.bAC.Size = New System.Drawing.Size(40, 40)
+        Me.bAC.TabIndex = 14
+        Me.bAC.Text = "AC"
+        Me.bAC.UseVisualStyleBackColor = True
+        '
+        'bDiv
+        '
+        Me.bDiv.Location = New System.Drawing.Point(229, 115)
+        Me.bDiv.Name = "bDiv"
+        Me.bDiv.Size = New System.Drawing.Size(40, 40)
+        Me.bDiv.TabIndex = 16
+        Me.bDiv.Text = "/"
+        Me.bDiv.UseVisualStyleBackColor = True
+        '
+        'bMulti
+        '
+        Me.bMulti.Location = New System.Drawing.Point(183, 115)
+        Me.bMulti.Name = "bMulti"
+        Me.bMulti.Size = New System.Drawing.Size(40, 40)
+        Me.bMulti.TabIndex = 15
+        Me.bMulti.Text = "x"
+        Me.bMulti.UseVisualStyleBackColor = True
+        '
+        'bMinus
+        '
+        Me.bMinus.Location = New System.Drawing.Point(229, 161)
+        Me.bMinus.Name = "bMinus"
+        Me.bMinus.Size = New System.Drawing.Size(40, 40)
+        Me.bMinus.TabIndex = 18
+        Me.bMinus.Text = "-"
+        Me.bMinus.UseVisualStyleBackColor = True
+        '
+        'bPlus
+        '
+        Me.bPlus.Location = New System.Drawing.Point(183, 161)
+        Me.bPlus.Name = "bPlus"
+        Me.bPlus.Size = New System.Drawing.Size(40, 40)
+        Me.bPlus.TabIndex = 17
+        Me.bPlus.Text = "+"
+        Me.bPlus.UseVisualStyleBackColor = True
+        '
+        'bEnter
+        '
+        Me.bEnter.Location = New System.Drawing.Point(183, 207)
+        Me.bEnter.Name = "bEnter"
+        Me.bEnter.Size = New System.Drawing.Size(86, 40)
+        Me.bEnter.TabIndex = 19
+        Me.bEnter.Text = "Enter"
+        Me.bEnter.UseVisualStyleBackColor = True
+        '
+        'bRound
+        '
+        Me.bRound.Location = New System.Drawing.Point(24, 267)
+        Me.bRound.Name = "bRound"
+        Me.bRound.Size = New System.Drawing.Size(86, 40)
+        Me.bRound.TabIndex = 20
+        Me.bRound.Text = "Round"
+        Me.bRound.UseVisualStyleBackColor = True
+        '
+        'b1x
+        '
+        Me.b1x.Location = New System.Drawing.Point(137, 267)
+        Me.b1x.Name = "b1x"
+        Me.b1x.Size = New System.Drawing.Size(40, 40)
+        Me.b1x.TabIndex = 21
+        Me.b1x.Text = "1/x"
+        Me.b1x.UseVisualStyleBackColor = True
+        '
+        'bPI
+        '
+        Me.bPI.Location = New System.Drawing.Point(183, 267)
+        Me.bPI.Name = "bPI"
+        Me.bPI.Size = New System.Drawing.Size(40, 40)
+        Me.bPI.TabIndex = 22
+        Me.bPI.Text = "PI"
+        Me.bPI.UseVisualStyleBackColor = True
+        '
+        'bE
+        '
+        Me.bE.Location = New System.Drawing.Point(229, 267)
+        Me.bE.Name = "bE"
+        Me.bE.Size = New System.Drawing.Size(40, 40)
+        Me.bE.TabIndex = 23
+        Me.bE.Text = "E"
+        Me.bE.UseVisualStyleBackColor = True
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(229, 50)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(39, 13)
+        Me.lbl1.TabIndex = 24
+        Me.lbl1.Text = "Label1"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(24, 50)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(39, 13)
+        Me.lbl2.TabIndex = 25
+        Me.lbl2.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(296, 347)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.bE)
+        Me.Controls.Add(Me.bPI)
+        Me.Controls.Add(Me.b1x)
+        Me.Controls.Add(Me.bRound)
+        Me.Controls.Add(Me.bEnter)
+        Me.Controls.Add(Me.bMinus)
+        Me.Controls.Add(Me.bPlus)
+        Me.Controls.Add(Me.bDiv)
+        Me.Controls.Add(Me.bMulti)
+        Me.Controls.Add(Me.bAC)
+        Me.Controls.Add(Me.bDel)
+        Me.Controls.Add(Me.b0)
+        Me.Controls.Add(Me.bDot)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Controls.Add(Me.b6)
+        Me.Controls.Add(Me.b5)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b9)
+        Me.Controls.Add(Me.b8)
+        Me.Controls.Add(Me.b7)
+        Me.Controls.Add(Me.tb)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tb As TextBox
+    Friend WithEvents b7 As Button
+    Friend WithEvents b8 As Button
+    Friend WithEvents b9 As Button
+    Friend WithEvents b6 As Button
+    Friend WithEvents b5 As Button
+    Friend WithEvents b4 As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b1 As Button
+    Friend WithEvents b0 As Button
+    Friend WithEvents bDot As Button
+    Friend WithEvents bDel As Button
+    Friend WithEvents bAC As Button
+    Friend WithEvents bDiv As Button
+    Friend WithEvents bMulti As Button
+    Friend WithEvents bMinus As Button
+    Friend WithEvents bPlus As Button
+    Friend WithEvents bEnter As Button
+    Friend WithEvents bRound As Button
+    Friend WithEvents b1x As Button
+    Friend WithEvents bPI As Button
+    Friend WithEvents bE As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+End Class
---
+++ Taschenrechner/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+
+    Public Property zahl As String = ""
+    Public Property calc As String = ""
+
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        tb.Text += "1"
+    End Sub
+
+    Private Sub B5_Click(sender As Object, e As EventArgs) Handles b5.Click
+        tb.Text += "5"
+    End Sub
+
+    Private Sub B3_Click(sender As Object, e As EventArgs) Handles b3.Click
+        tb.Text += "3"
+    End Sub
+
+    Private Sub B6_Click(sender As Object, e As EventArgs) Handles b6.Click
+        tb.Text += "6"
+    End Sub
+
+    Private Sub B2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        tb.Text += "2"
+    End Sub
+
+    Private Sub B4_Click(sender As Object, e As EventArgs) Handles b4.Click
+        tb.Text += "4"
+    End Sub
+
+    Private Sub B7_Click(sender As Object, e As EventArgs) Handles b7.Click
+        tb.Text += "7"
+    End Sub
+
+    Private Sub B8_Click(sender As Object, e As EventArgs) Handles b8.Click
+        tb.Text += "8"
+    End Sub
+
+    Private Sub B9_Click(sender As Object, e As EventArgs) Handles b9.Click
+        tb.Text += "9"
+    End Sub
+
+    Private Sub B0_Click(sender As Object, e As EventArgs) Handles b0.Click
+        tb.Text += "0"
+    End Sub
+
+    Private Sub BDel_Click(sender As Object, e As EventArgs) Handles bDel.Click
+        If (tb.Text.Length > 0) Then
+            tb.Text = tb.Text.Substring(0, tb.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub BAC_Click(sender As Object, e As EventArgs) Handles bAC.Click
+        calc = ""
+        zahl = ""
+        tb.Text = ""
+
+        lbl1.Text = ""
+        lbl2.Text = ""
+    End Sub
+
+    Private Sub BMulti_Click(sender As Object, e As EventArgs) Handles bMulti.Click
+        zahl = tb.Text
+        lbl2.Text = zahl
+        tb.Text = ""
+
+        calc = "x"
+        lbl1.Text = calc
+    End Sub
+
+    Private Sub BDiv_Click(sender As Object, e As EventArgs) Handles bDiv.Click
+        zahl = tb.Text
+        lbl2.Text = zahl
+        tb.Text = ""
+
+        calc = "/"
+        lbl1.Text = calc
+    End Sub
+
+    Private Sub BPlus_Click(sender As Object, e As EventArgs) Handles bPlus.Click
+        zahl = tb.Text
+        lbl2.Text = zahl
+        tb.Text = ""
+
+        calc = "+"
+        lbl1.Text = calc
+    End Sub
+
+    Private Sub BMinus_Click(sender As Object, e As EventArgs) Handles bMinus.Click
+        zahl = tb.Text
+        lbl2.Text = zahl
+        tb.Text = ""
+
+        calc = "-"
+        lbl1.Text = calc
+    End Sub
+
+    Private Sub BDot_Click(sender As Object, e As EventArgs) Handles bDot.Click
+        tb.Text += ","
+    End Sub
+
+    Private Sub BEnter_Click(sender As Object, e As EventArgs) Handles bEnter.Click
+        If (calc = "+") Then
+            tb.Text = Val(zahl) + Val(tb.Text)
+        End If
+        If (calc = "-") Then
+            tb.Text = Val(zahl) - Val(tb.Text)
+        End If
+        If (calc = "x") Then
+            tb.Text = Val(zahl) * Val(tb.Text)
+        End If
+        If (calc = "/") Then
+            tb.Text = Val(zahl) / Val(tb.Text)
+        End If
+
+        lbl1.Text = ""
+        lbl2.Text = ""
+    End Sub
+
+    Private Sub BRound_Click(sender As Object, e As EventArgs) Handles bRound.Click
+        tb.Text = Math.Round(Val(tb.Text))
+    End Sub
+
+    Private Sub B1x_Click(sender As Object, e As EventArgs) Handles b1x.Click
+        tb.Text = 1.0 / Val(tb.Text)
+    End Sub
+
+    Private Sub BPI_Click(sender As Object, e As EventArgs) Handles bPI.Click
+        tb.Text = Math.PI
+    End Sub
+
+    Private Sub BE_Click(sender As Object, e As EventArgs) Handles bE.Click
+        tb.Text = Math.E
+    End Sub
+End Class
